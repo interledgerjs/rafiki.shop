17d86e9089cd3351e7d858f3fa8fd932ce4d3a6d
Matt de Haast <matt.dehaast@coil.com>
2019-11-12 14:42:51 +0200

    working checkout demo with automated STREAM payment with bugs fixed and dockerized

--- sites/checkout/next.config.js
+++ sites/checkout/next.config.js
@@ -1,8 +1,9 @@
 const withCSS = require('@zeit/next-css')
+require("dotenv").config();
 
 module.exports = withCSS({
     env: {
-        OAUTH_CLIENT_ID: 'eats.rafiki.shop',
-        OAUTH_CALLBACK_URL: 'http://localhost:3000/callback'
+        OAUTH_CLIENT_ID: process.env.OAUTH_CLIENT_ID || 'eats.rafiki.shop',
+        OAUTH_CALLBACK_URL: process.env.OAUTH_CALLBACK_URL || 'https://eats.rafiki.shop/callback'
     }
 });
