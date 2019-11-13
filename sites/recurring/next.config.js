d576f5fb40e6e8de9404b13c3b9001e99527dd73
talpat <talonpatterson94@gmail.com>
2019-11-13 14:58:09 +0200

    independent recurring

--- sites/recurring/next.config.js
+++ sites/recurring/next.config.js
@@ -2,7 +2,7 @@ const withCSS = require('@zeit/next-css')
 
 module.exports = withCSS({
     env: {
-        OAUTH_CLIENT_ID: 'flix.rafiki.shop',
+        OAUTH_CLIENT_ID: 'rafiki.shop',
         OAUTH_CALLBACK_URL: 'http://localhost:3000/callback'
     }
 });
